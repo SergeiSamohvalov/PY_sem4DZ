c566dd2c9862792c0b015dbf56f2d2340e246196
sergei sam <ssi1970@mail.ru>
2022-09-21 09:17:14 +0700

    added new task_3

--- task_3.py
+++ task_3.py
@@ -1,13 +1,29 @@
 # Задайте последовательность чисел.
 # Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
 
-list_1 = [7, 2, 5, 4, 7, 3, 6, 2, 5, 8, 13, 8]
+# list_1 = [7, 2, 5, 4, 7, 3, 6, 2, 5, 8, 13, 8]
 
-def get_unique_numbers(list_1):
-    list_of_unique_numbers = []
-    unique_numbers = set(list_1)
-    for number in unique_numbers:
-        list_of_unique_numbers.append(number)
-    return list_of_unique_numbers
+# def get_unique_numbers(list_1):
+#     list_of_unique_numbers = []
+#     unique_numbers = set(list_1)
+#     for number in unique_numbers:
+#         list_of_unique_numbers.append(number)
+#     return list_of_unique_numbers
 
-print(get_unique_numbers(list_1))
+# print(get_unique_numbers(list_1))
+
+# for i in range(len(userList)):
+#     for j in range(len(userList)):
+#         if i != j and userList[i] == userList[j]:
+#             break
+#     else:
+#         uniqueList.append(userList[i])
+
+
+b = [1, 1, 2, 3, 3, 4, 5]
+a = []
+for i in b:
+    if b.count(i) == 1:
+        a.append(i)
+
+        print(a)
